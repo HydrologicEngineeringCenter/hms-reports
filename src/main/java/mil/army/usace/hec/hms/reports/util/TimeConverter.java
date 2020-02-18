190527c410cec5eeb07dbad3131bd5d0d35cfa79
Nick Van <Nghiem.T.Van@usace.army.mil>
2020-02-18 15:40:31 -0800

    Add logic to reformat date

--- src/main/java/mil/army/usace/hec/hms/reports/util/TimeConverter.java
+++ src/main/java/mil/army/usace/hec/hms/reports/util/TimeConverter.java
@@ -2,6 +2,7 @@ package mil.army.usace.hec.hms.reports.util;
 
 import hec.heclib.util.HecTime;
 
+import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
 import java.util.Date;
 
@@ -9,11 +10,18 @@ public class TimeConverter {
 
     private TimeConverter(){}
 
-    public static ZonedDateTime toZonedDateTime(HecTime hecTime){
+    public static ZonedDateTime toZonedDateTime(HecTime hecTime) {
         return ZonedDateTime.parse(hecTime.getXMLDateTime(0));
     }
 
-    public static HecTime toHecTime(ZonedDateTime zonedDateTime){
+    public static HecTime toHecTime(ZonedDateTime zonedDateTime) {
         return new HecTime(Date.from(zonedDateTime.toInstant()), 0);
+    }
+
+    public static String toString(ZonedDateTime zonedDateTime, String dateFormat) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        Date toDate = Date.from(zonedDateTime.toInstant());
+
+        return simpleDateFormat.format(toDate);
     }
 }
