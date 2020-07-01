1bab822caed3a56a82f819d7094cf94449eb4802
Nick Van <Nghiem.T.Van@usace.army.mil>
2020-07-01 10:31:18 -0700

    Add some tests

--- src/test/java/mil/army/usace/hec/hms/reports/io/StandardReportWriterTest.java
+++ src/test/java/mil/army/usace/hec/hms/reports/io/StandardReportWriterTest.java
@@ -3,6 +3,7 @@ package mil.army.usace.hec.hms.reports.io;
 import mil.army.usace.hec.hms.reports.Element;
 import mil.army.usace.hec.hms.reports.enums.ParameterSummary;
 import mil.army.usace.hec.hms.reports.enums.ReportWriterType;
+import mil.army.usace.hec.hms.reports.enums.SimulationType;
 import mil.army.usace.hec.hms.reports.enums.SummaryChoice;
 import mil.army.usace.hec.hms.reports.util.Utilities;
 import org.junit.jupiter.api.Test;
@@ -33,11 +34,10 @@ class StandardReportWriterTest {
 
     @Test
     void writeLong() {
-//        String pathToInput  = "src/resources/MiddleColumbia/MiddleColumbia_WY2017.basin.json";
-        String pathToInput  = "C:\\Users\\q0hecntv\\Desktop\\MiddleColumbiaForNick\\MiddleColumbia\\MiddleColumbia_WY2017.basin";
-        String pathToResult = "src/resources/MiddleColumbia/RUN_WY2017.results";
-        String pathToOutput = "src/resources/output-long.html";
-        String projectDir   = "C:\\Users\\q0hecntv\\Desktop\\MiddleColumbiaForNick\\MiddleColumbia";
+        String pathToInput  = "C:\\Users\\q0hecntv\\Desktop\\HmsReports\\MiddleColumbia_forNick\\MiddleColumbia\\MiddleColumbia_WY1997.basin";
+        String pathToResult = "C:\\Users\\q0hecntv\\Desktop\\HmsReports\\MiddleColumbia_forNick\\MiddleColumbia\\results\\RUN_WY1997.results";
+        String pathToOutput = "C:\\Temp\\reportTest.html";
+        String projectDir   = "C:\\Users\\q0hecntv\\Desktop\\HmsReports\\MiddleColumbia_forNick\\MiddleColumbia";
 
         Map<String, List<String>> availableGlobalParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.GLOBAL_PARAMETER);
         Map<String, List<String>> availableElementParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.ELEMENT_PARAMETER);
@@ -54,6 +54,8 @@ class StandardReportWriterTest {
                 .globalParameterChoices(availableGlobalParameter)
                 .elementParameterizationChoice(availableElementParameter)
                 .reportWriterType(ReportWriterType.STANDARD_REPORT)
+                .simulationType(SimulationType.RUN)
+                .chosenPlots(Utilities.getAvailablePlot(pathToResult))
                 .build();
 
         List<Element> elementList = reportWriter.write();
@@ -61,7 +63,6 @@ class StandardReportWriterTest {
 
     @Test
     void writePunx() {
-//        String pathToInput = "C:\\HyperNick\\Punx\\punxsutawney.basin.json";
         String pathToInput = "C:\\HyperNick\\Punx\\punxsutawney.basin";
         String pathToResult = "C:\\HyperNick\\Punx\\results\\RUN_Sep_2018.results";
         String pathToDestination = "C:\\HyperNick\\HmsReportOutput\\hms-output.html";
@@ -70,23 +71,6 @@ class StandardReportWriterTest {
         /* The user choosing what global parameters to hide/not print out */
         Map<String, List<String>> availableGlobalParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.GLOBAL_PARAMETER);
         Map<String, List<String>> availableElementParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.ELEMENT_PARAMETER);
-
-        /* Removing Element Parameters */
-//        availableElementParameter.remove("Subbasin");
-//        availableElementParameter.remove("Reach");
-
-//        availableElementParameter.get("Subbasin").remove("Loss Rate");
-
-        /* Removing Global Parameters */
-//        availableGlobalParameter.remove("Subbasin");
-//        availableGlobalParameter.remove("Reach");
-
-//        availableGlobalParameter.get("Subbasin").remove("Area");
-//        availableGlobalParameter.get("Subbasin").remove("Loss Rate");
-//        availableGlobalParameter.get("Subbasin").remove("Canopy");
-//        availableGlobalParameter.get("Subbasin").remove("Transform");
-//        availableGlobalParameter.get("Subbasin").remove("Baseflow");
-//        availableGlobalParameter.get("Reach").remove("Route");
 
         ReportWriter reportWriter = ReportWriter.builder()
                 .pathToInput(pathToInput)
@@ -97,8 +81,145 @@ class StandardReportWriterTest {
                 .elementParameterizationChoice(availableElementParameter)
                 .globalParameterChoices(availableGlobalParameter)
                 .reportWriterType(ReportWriterType.STANDARD_REPORT)
+                .simulationType(SimulationType.RUN)
                 .build();
 
         List<Element> elementList = reportWriter.write();
     }
+
+    @Test
+    void writeForecast() {
+        String pathToInput = "C:\\Projects\\hec-hms\\src\\test\\resources\\forecast\\Base.basin";
+        String pathToResult = "C:\\Projects\\hec-hms\\src\\test\\resources\\forecast\\results\\FOR_05_06_Forecast.results";
+        String pathToDestination = "C:\\HyperNick\\HmsReportOutput\\Report-Forecast.html";
+        String projectDirectory = "C:\\Projects\\hec-hms\\src\\test\\resources\\forecast";
+
+        /* The user choosing what global parameters to hide/not print out */
+        Map<String, List<String>> availableGlobalParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.GLOBAL_PARAMETER);
+        Map<String, List<String>> availableElementParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.ELEMENT_PARAMETER);
+
+        ReportWriter reportWriter = ReportWriter.builder()
+                .pathToInput(pathToInput)
+                .pathToResult(pathToResult)
+                .pathToDestination(pathToDestination)
+                .projectDirectory(projectDirectory)
+                .reportSummaryChoice(Arrays.asList(SummaryChoice.GLOBAL_RESULTS_SUMMARY, SummaryChoice.GLOBAL_PARAMETER_SUMMARY, SummaryChoice.ELEMENT_RESULTS_SUMMARY))
+                .elementParameterizationChoice(availableElementParameter)
+                .globalParameterChoices(availableGlobalParameter)
+                .reportWriterType(ReportWriterType.STANDARD_REPORT)
+                .simulationType(SimulationType.FORECAST)
+                .build();
+
+        List<Element> elementList = reportWriter.write();
+        System.out.println("Done");
+    }
+
+    @Test
+    void writeOptimization() {
+        String pathToInput = "C:\\Projects\\hec-hms\\src\\test\\resources\\optimization\\Punxsutawney.basin";
+        String pathToResult = "C:\\Projects\\hec-hms\\src\\test\\resources\\optimization\\results\\OPT_Opt_May_95.results";
+        String pathToDestination = "C:\\HyperNick\\HmsReportOutput\\Report-Optimization.html";
+        String projectDirectory = "C:\\Projects\\hec-hms\\src\\test\\resources\\optimization";
+
+        /* The user choosing what global parameters to hide/not print out */
+        Map<String, List<String>> availableGlobalParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.GLOBAL_PARAMETER);
+        Map<String, List<String>> availableElementParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.ELEMENT_PARAMETER);
+
+        ReportWriter reportWriter = ReportWriter.builder()
+                .pathToInput(pathToInput)
+                .pathToResult(pathToResult)
+                .pathToDestination(pathToDestination)
+                .projectDirectory(projectDirectory)
+                .reportSummaryChoice(Arrays.asList(SummaryChoice.GLOBAL_RESULTS_SUMMARY, SummaryChoice.GLOBAL_PARAMETER_SUMMARY, SummaryChoice.ELEMENT_RESULTS_SUMMARY))
+                .elementParameterizationChoice(availableElementParameter)
+                .globalParameterChoices(availableGlobalParameter)
+                .reportWriterType(ReportWriterType.STANDARD_REPORT)
+                .simulationType(SimulationType.OPTIMIZATION)
+                .build();
+
+        List<Element> elementList = reportWriter.write();
+        System.out.println("Done");
+    }
+
+    @Test
+    void writeRun() {
+        String pathToInput = "C:\\Projects\\hec-hms\\src\\test\\resources\\castro\\Castro_1.basin";
+        String pathToResult = "C:\\Projects\\hec-hms\\src\\test\\resources\\castro\\results\\RUN_Current.results";
+        String pathToDestination = "C:\\HyperNick\\HmsReportOutput\\Report-Run.html";
+        String projectDirectory = "C:\\Projects\\hec-hms\\src\\test\\resources\\castro";
+
+        /* The user choosing what global parameters to hide/not print out */
+        Map<String, List<String>> availableGlobalParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.GLOBAL_PARAMETER);
+        Map<String, List<String>> availableElementParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.ELEMENT_PARAMETER);
+
+        ReportWriter reportWriter = ReportWriter.builder()
+                .pathToInput(pathToInput)
+                .pathToResult(pathToResult)
+                .pathToDestination(pathToDestination)
+                .projectDirectory(projectDirectory)
+                .reportSummaryChoice(Arrays.asList(SummaryChoice.GLOBAL_RESULTS_SUMMARY, SummaryChoice.GLOBAL_PARAMETER_SUMMARY, SummaryChoice.ELEMENT_RESULTS_SUMMARY))
+                .elementParameterizationChoice(availableElementParameter)
+                .globalParameterChoices(availableGlobalParameter)
+                .reportWriterType(ReportWriterType.STANDARD_REPORT)
+                .simulationType(SimulationType.RUN)
+                .build();
+
+        List<Element> elementList = reportWriter.write();
+        System.out.println("Done");
+    }
+
+    @Test
+    void writeMonteCarlo() {
+        String pathToInput = "C:\\Projects\\hec-hms\\src\\test\\resources\\montecarlo\\Simple.basin";
+        String pathToResult = "C:\\Projects\\hec-hms\\src\\test\\resources\\montecarlo\\results\\MCA_Normal.results";
+        String pathToDestination = "C:\\HyperNick\\HmsReportOutput\\Report-MonteCarlo.html";
+        String projectDirectory = "C:\\Projects\\hec-hms\\src\\test\\resources\\montecarlo";
+
+        /* The user choosing what global parameters to hide/not print out */
+        Map<String, List<String>> availableGlobalParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.GLOBAL_PARAMETER);
+        Map<String, List<String>> availableElementParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.ELEMENT_PARAMETER);
+
+        ReportWriter reportWriter = ReportWriter.builder()
+                .pathToInput(pathToInput)
+                .pathToResult(pathToResult)
+                .pathToDestination(pathToDestination)
+                .projectDirectory(projectDirectory)
+                .reportSummaryChoice(Arrays.asList(SummaryChoice.GLOBAL_RESULTS_SUMMARY, SummaryChoice.GLOBAL_PARAMETER_SUMMARY, SummaryChoice.ELEMENT_RESULTS_SUMMARY))
+                .elementParameterizationChoice(availableElementParameter)
+                .globalParameterChoices(availableGlobalParameter)
+                .reportWriterType(ReportWriterType.STANDARD_REPORT)
+                .simulationType(SimulationType.MONTE_CARLO)
+                .build();
+
+        List<Element> elementList = reportWriter.write();
+        System.out.println("Done");
+    }
+
+    @Test
+    void writeDepthArea() {
+        String pathToInput = "C:\\Projects\\hec-hms\\src\\test\\resources\\deptharea\\Subset.basin";
+        String pathToResult = "C:\\Projects\\hec-hms\\src\\test\\resources\\deptharea\\results\\DAA_Maricopa.results";
+        String pathToDestination = "C:\\HyperNick\\HmsReportOutput\\Report-DepthArea.html";
+        String projectDirectory = "C:\\Projects\\hec-hms\\src\\test\\resources\\deptharea";
+
+        /* The user choosing what global parameters to hide/not print out */
+        Map<String, List<String>> availableGlobalParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.GLOBAL_PARAMETER);
+        Map<String, List<String>> availableElementParameter = Utilities.getParameterMap(pathToInput, ParameterSummary.ELEMENT_PARAMETER);
+
+        ReportWriter reportWriter = ReportWriter.builder()
+                .pathToInput(pathToInput)
+                .pathToResult(pathToResult)
+                .pathToDestination(pathToDestination)
+                .projectDirectory(projectDirectory)
+                .reportSummaryChoice(Arrays.asList(SummaryChoice.GLOBAL_RESULTS_SUMMARY, SummaryChoice.GLOBAL_PARAMETER_SUMMARY, SummaryChoice.ELEMENT_RESULTS_SUMMARY))
+                .elementParameterizationChoice(availableElementParameter)
+                .globalParameterChoices(availableGlobalParameter)
+                .reportWriterType(ReportWriterType.STANDARD_REPORT)
+                .simulationType(SimulationType.DEPTH_AREA)
+                .build();
+
+        List<Element> elementList = reportWriter.write();
+        System.out.println("Done");
+    }
+
 }
